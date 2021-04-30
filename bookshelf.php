8be20de95e6f1d7ef8bde7ba6d382fd0d4c98530
YNGREDH COSTA DA CRUZ <yngredh.costa@gmail.com>
2021-04-29 23:01:50 -0300

    Adição da pagina da estante + css

---
+++ bookshelf.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@600&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@600&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/bookshelf.css">
+    <link rel="stylesheet" href="css/top-bar.css">
+    <link rel="stylesheet" href="css/footer.css">
+    <link rel="icon" type="image/x-icon" href="imagens/booklogo.png">
+    <title>Estante dos Sonhos</title>
+</head>
+
+<body>
+    <!-- BARRA SUPERIOR -->
+    <header>
+        <ul>
+            <li id="logo-box">
+                <a id="link-menu-logo" href="index.php"><img id="logo" src="imagens/Logo.png" alt=""></a>
+            </li>
+            <div id="saudacao-icones">
+                <div id="menu-superior">
+                    <div id="saudacao">
+                        <p>Olá, Davi</p>
+                    </div>
+                    <div id="pesquisa-carrinho">
+                        <input type="text" placeholder="Pesquisar" name="pesquisar" id="barra-pesquisa">
+                        <div id="botoes-menu">
+                            <li id="Carrinho">
+                                <a id="link-menu" href="carrinho.php"><img id="img-carrinho" src="imagens/carinho.png" alt="Carrinho"></a>
+                            </li>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <li id="barra-navegacao">
+                <div id="links">
+                    <a href="index.php">
+                        INÍCIO
+                    </a>
+
+                    <a href="store.php">
+                        LOJA
+                    </a>
+
+                    <a href="profile.php">
+                        PERFIL
+                    </a>
+
+                    <a href="bookshelf.php">
+                        ESTANTE<br>DOS SONHOS
+                    </a>
+                </div>
+            </li>
+        </ul>
+    </header>
+    <!-- BARRA SUPERIOR -->
+
+    <section id="shelf">
+        <article class="grid-shelf">
+            <div>
+                <div class="shelf-icon">
+                    <!-- <img src="fahrenheit_451.jpg" alt=""> -->
+                </div>
+                <div class="shelf-button">
+                    <div class="buy-button"><a href="">
+                            <p>COMPRAR</p>
+                        </a></div>
+                    <div class="remove-button"><a href="">
+                            <p>REMOVER</p>
+                        </a></div>
+                </div>
+            </div>
+        </article>
+    </section>
+
+    <!-- FOOTER -->
+    <footer>
+        <div class="top-footer">
+            <div id="mobile-info">
+                <h2>Mobile app</h2>
+                <div>
+                    <p><a href="">Baixar agora para Android</a></p>
+                    <p><a href="">Baixar agora para IOS</a></p>
+                </div>
+            </div>
+            <div id="about-info">
+                <h2>About Us</h2>
+                <div>
+                    <p><a href="http://www.github.com/Yngredh" target="_BLANK">github.com/Yngredh</a></p>
+                    <p><a href="http://www.github.com/davihenrique05" target="_BLANK">github.com/davihenrique05</a></p>
+                    <p><a href="http://www.github.com/AdalbertoTeshimaJunior" target="_BLANK">github.com/AdalbertoTeshimaJunior</a></p>
+                </div>
+            </div>
+            <div id="contact-info">
+                <h2>Contact Us</h2>
+                <div>
+                    <p><a href="http://www.linkedin.com/in/yngredh-cruz" target="_BLANK">linkedin.com/in/yngredh-cruz</a></p>
+                    <p><a href="http://www.linkedin.com/in/davi-hg-silva" target="_BLANK">linkedin.com/in/davi-hg-silva</a></p>
+                    <p><a href="http://www.linkedin.com/in/adalberto-teshima-júnior" target="_BLANK">linkedin.com/in/adalberto-teshima-júnior</a></p>
+                </div>
+            </div>
+        </div>
+        <hr>
+        <div class="bottom-footer">
+            <p id="assinatura">© BookStack, Inc. 2021. We love books!</p>
+            <div id="social-media">
+                <p>Follow us: </p>
+                <img src="imagens/instagram-icone.png">
+                <img src="imagens/twitter-icone.png">
+                <img src="imagens/facebook-icone.png">
+            </div>
+        </div>
+    </footer>
+    <!-- FOOTER -->
+</body>
+
+</html>
